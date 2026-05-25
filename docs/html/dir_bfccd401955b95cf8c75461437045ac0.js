fea233da24c065c442a94d685d76bf83b825754d
Marco Lasagna <marco.lasagna@studenti.unitn.it>
2026-05-25 10:17:35 +0200

    Moved mspm0l2228_mpu_config folder inside mspm0l2228 folder

--- docs/html/dir_bfccd401955b95cf8c75461437045ac0.js
+++ docs/html/dir_bfccd401955b95cf8c75461437045ac0.js
@@ -1,6 +1,6 @@
 var dir_bfccd401955b95cf8c75461437045ac0 =
 [
-    [ "mspm0l2228_mpu_config", "dir_105427749de46d5cdc9bacff9d01429a.html", "dir_105427749de46d5cdc9bacff9d01429a" ],
+    [ "mspm0l2228", "dir_c1092a27904c63a58d1037dbb505c070.html", "dir_c1092a27904c63a58d1037dbb505c070" ],
     [ "chiparmor.h", "da/df2/chiparmor_8h_source.html", null ],
     [ "chiparmour.h", "d1/d9e/chiparmour_8h_source.html", null ]
 ];
---
+++ docs/html/dir_ddb103ba6fbf4209f8b03e1f12fcc376.js
@@ -0,0 +1,4 @@
+var dir_ddb103ba6fbf4209f8b03e1f12fcc376 =
+[
+    [ "mpu.h", "db/d0d/mpu_8h_source.html", null ]
+];
